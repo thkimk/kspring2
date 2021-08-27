0ad4f357924db182703613e57817ac9a530c4188
DESKTOP-49P9B8Q\김태헌 <0707thkim@gmail.com>
2021-08-27 20:24:30 +0900

    최초 커밋

---
+++ src/test/java/com/kkk/kspring2/Kspring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkk.kspring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kspring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
