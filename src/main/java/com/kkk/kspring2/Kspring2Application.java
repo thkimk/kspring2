80471a56a545b44043d9bbf243a398b4a10f7704
DESKTOP-49P9B8Q\김태헌 <0707thkim@gmail.com>
2021-08-27 21:51:07 +0900

    변경사항 반영

---
+++ src/main/java/com/kkk/kspring2/Kspring2Application.java
@@ -0,0 +1,13 @@
+package com.kkk.kspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kspring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kspring2Application.class, args);
+	}
+
+}
